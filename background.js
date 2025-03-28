2ff7c5c9dcc2c747d47373df1dbb34a8f83d6736
Jonathan Simcoe <275617+jdsimcoe@users.noreply.github.com>
2025-03-28 09:22:03 -0700

    Updating for Chrome Manifest v3

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(tab.id, {file: "bookmarklet.js"})
+chrome.action.onClicked.addListener(function (tab) {
+  const pinboardUrl = `https://pinboard.in/add?next=close&url=${encodeURIComponent(tab.url)}&title=${encodeURIComponent(tab.title)}&description=${encodeURIComponent(tab.title)}`;
+
+  chrome.windows.create({
+    url: pinboardUrl,
+    type: "popup",
+    width: 600,
+    height: 550,
+    focused: true,
+  });
 });
